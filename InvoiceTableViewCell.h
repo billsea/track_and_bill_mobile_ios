447a4aae0fdf2f2a1526fd1400246c16864429e3
Bill S <bill.e.seaman@gmail.com>
2018-05-02 10:53:38 -0700

    Add invoices table view cell

---
+++ InvoiceTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  InvoiceTableViewCell.h
+//  trackandbill_ios
+//
+//  Created by Loud on 5/2/18.
+//  Copyright © 2018 loudsoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InvoiceTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *invoiceNumber;
+@property (weak, nonatomic) IBOutlet UILabel *invoiceDate;
+
+@end
---
+++ InvoiceTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  InvoiceTableViewCell.m
+//  trackandbill_ios
+//
+//  Created by Loud on 5/2/18.
+//  Copyright © 2018 loudsoftware. All rights reserved.
+//
+
+#import "InvoiceTableViewCell.h"
+
+@implementation InvoiceTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
