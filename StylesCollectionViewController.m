6e4067211819094231a3a737bbed28e1e843a10d
Bill S <bill.e.seaman@gmail.com>
2018-05-02 10:54:30 -0700

    Update background color

--- StylesCollectionViewController.m
+++ StylesCollectionViewController.m
@@ -19,8 +19,7 @@ static NSString * const reuseIdentifier = @"Cell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.title = NSLocalizedString(@"styles", nil);
     
     // Register cell classes
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
