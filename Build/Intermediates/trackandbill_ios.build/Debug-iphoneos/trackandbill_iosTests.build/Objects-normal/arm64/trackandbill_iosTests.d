327512f82934cc38edb91f29f991c0a87c72b69c
Bill S <bill.e.seaman@gmail.com>
2018-06-01 17:33:33 -0700

    add build files

--- Build/Intermediates/trackandbill_ios.build/Debug-iphoneos/trackandbill_iosTests.build/Objects-normal/arm64/trackandbill_iosTests.d
+++ Build/Intermediates/trackandbill_ios.build/Debug-iphoneos/trackandbill_iosTests.build/Objects-normal/arm64/trackandbill_iosTests.d
@@ -1,2 +1,5 @@
 dependencies: \
-  /Users/Bill/Documents/iOS_Projects/track_and_bill_mobile_ios/trackandbill_iosTests/trackandbill_iosTests.m
+  /Users/Bill/Documents/iOS_Projects/track_and_bill_mobile_ios/trackandbill_iosTests/trackandbill_iosTests.m \
+  /Users/Bill/Documents/iOS_Projects/track_and_bill_mobile_ios/DashboardCollectionViewController.h \
+  /Users/Bill/Documents/iOS_Projects/track_and_bill_mobile_ios/ClientsTableViewController.h \
+  /Users/Bill/Documents/iOS_Projects/track_and_bill_mobile_ios/ProfileTableViewController.h
